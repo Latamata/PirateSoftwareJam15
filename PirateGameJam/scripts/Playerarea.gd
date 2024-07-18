a8a249cfa7e5e88171f6ecfea1aeaad793cd7486
Jefferson Webber <chancewebber@gmail.com>
2024-07-17 21:39:51 -0700

    first day upload of Godot project and game design doc

---
+++ PirateGameJam/scripts/Playerarea.gd
@@ -0,0 +1,28 @@
+extends Area2D
+
+var top_left
+var bottom_right
+
+func _ready():
+	update_boundaries()
+
+func update_boundaries():
+	var collision_shape = $CollisionShape2D
+	var shape = collision_shape.shape
+
+	if shape is RectangleShape2D:
+		var global_pos = collision_shape.global_position
+		var extents = shape.extents
+
+		# Calculate the corners of the RectangleShape2D
+		top_left = global_pos - extents
+		bottom_right = global_pos + extents
+		print("Top Left: ", top_left)
+		print("Bottom Right: ", bottom_right)
+	else:
+		print("Shape is not a RectangleShape2D")
+	
+
+
+func _on_body_entered(_body):
+	update_boundaries()
---
+++ PirateGameJam/scripts/player.gd
@@ -0,0 +1,25 @@
+extends CharacterBody2D
+
+
+@export var max_speed: int = 100
+var speed: int = max_speed
+@onready var boundary_area = get_parent().get_node("Playerarea")
+
+func confine_to_boundary():
+	var boundary_shape = boundary_area.get_node("CollisionShape2D").shape
+	if boundary_shape is RectangleShape2D:
+		var top_left = boundary_area.top_left
+		var bottom_right = boundary_area.bottom_right
+		# Constrain the player position within the boundaries
+		global_position.x = clamp(global_position.x, top_left.x, bottom_right.x)
+		global_position.y = clamp(global_position.y, top_left.y, bottom_right.y)
+
+func _process(delta):	
+	var direction = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	velocity = direction * speed
+	position += direction * speed * delta
+	#look_at(get_global_mouse_position())
+	move_and_slide()
+	confine_to_boundary()
+
+
